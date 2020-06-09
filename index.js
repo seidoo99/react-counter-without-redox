87556155f91ec00096fa0da77db34ff29cb039e9
seid zerihun <seidoo99@gmial.com>
2020-06-09 17:50:31 -0400

    counter function react

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-ReactDOM.render(<div>implement a counter and render it here!</div>, document.getElementById('root'))
+class Counter extends React.Component  {
+    constructor(props){
+        super(props);
+    this.state = {
+        counter:0
+       };
+    }
+counterFunction = ()=> {
+    this.setState({
+        counter: this.state.counter += 1
+    })
+}
+
+render(){
+    return <div>
+    <button onClick = {this.counterFunction}>click me</button>
+    counter increases by {this.state.counter}
+    </div>
+  }
+}
+ReactDOM.render(<Counter/>, document.getElementById('root'))
